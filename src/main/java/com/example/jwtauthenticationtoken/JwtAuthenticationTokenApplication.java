0ea16849e33f51660725c3a208ced4c0d5eca0f2
Tushar <tushargarg0110@gmail.com>
2024-02-02 22:34:30 +0530

    JWT Authentication Token

---
+++ src/main/java/com/example/jwtauthenticationtoken/JwtAuthenticationTokenApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtauthenticationtoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthenticationTokenApplication.class, args);
+	}
+
+}
